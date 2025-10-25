122c65be4e37b549da2755bb22dabad9a29afa71
stagas <gstagas@gmail.com>
2025-10-25 17:24:34 +0300

    save

--- as/assembly/dsp.ts
+++ as/assembly/dsp.ts
@@ -2,11 +2,6 @@ export class Dsp {
   L: f32 = 0
   R: f32 = 0
 
-  constructor() {
-    this.L = 0
-    this.R = 0
-  }
-
   process(): void {
     const sample = Mathf.random() * 2.0 - 1.0
     this.L = sample
