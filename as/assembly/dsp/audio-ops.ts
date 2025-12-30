ee24b75ce589b523e05a689b836e2521d7411b56
stagas <gstagas@gmail.com>
2025-12-30 16:13:48 +0200

    save

--- as/assembly/dsp/audio-ops.ts
+++ as/assembly/dsp/audio-ops.ts
@@ -3,65 +3,67 @@
 // @ts-ignore
 // @inline
 export function clearAudio(out$: usize, length: i32): void {
-  for (let i = 0; i < length; i++) {
-    store<f32>(out$, 0)
-    out$ += 4
+  const len = length >> 2
+  const zero = f32x4.splat(0)
+  for (let i = 0; i < len; i++) {
+    v128.store(out$, zero)
+    out$ += 16
   }
 }
 
 // @ts-ignore
 // @inline
 export function addAudio(out$: usize, a1$: usize, a2$: usize, length: i32): void {
-  for (let i = 0; i < length; i++) {
-    const s1 = load<f32>(a1$)
-    const s2 = load<f32>(a2$)
-    const sample = s1 + s2
-    store<f32>(out$, sample)
-    out$ += 4
-    a1$ += 4
-    a2$ += 4
+  const len = length >> 2
+  for (let i = 0; i < len; i++) {
+    const v1 = v128.load(a1$)
+    const v2 = v128.load(a2$)
+    v128.store(out$, f32x4.add(v1, v2))
+    out$ += 16
+    a1$ += 16
+    a2$ += 16
   }
 }
 
 // @ts-ignore
 // @inline
 export function subAudio(out$: usize, a1$: usize, a2$: usize, length: i32): void {
-  for (let i = 0; i < length; i++) {
-    const s1 = load<f32>(a1$)
-    const s2 = load<f32>(a2$)
-    const sample = s1 - s2
-    store<f32>(out$, sample)
-    out$ += 4
-    a1$ += 4
-    a2$ += 4
+  const len = length >> 2
+  for (let i = 0; i < len; i++) {
+    const v1 = v128.load(a1$)
+    const v2 = v128.load(a2$)
+    v128.store(out$, f32x4.sub(v1, v2))
+    out$ += 16
+    a1$ += 16
+    a2$ += 16
   }
 }
 
 // @ts-ignore
 // @inline
 export function mulAudio(out$: usize, a1$: usize, a2$: usize, length: i32): void {
-  for (let i = 0; i < length; i++) {
-    const s1 = load<f32>(a1$)
-    const s2 = load<f32>(a2$)
-    const sample = s1 * s2
-    store<f32>(out$, sample)
-    out$ += 4
-    a1$ += 4
-    a2$ += 4
+  const len = length >> 2
+  for (let i = 0; i < len; i++) {
+    const v1 = v128.load(a1$)
+    const v2 = v128.load(a2$)
+    v128.store(out$, f32x4.mul(v1, v2))
+    out$ += 16
+    a1$ += 16
+    a2$ += 16
   }
 }
 
 // @ts-ignore
 // @inline
 export function divAudio(out$: usize, a1$: usize, a2$: usize, length: i32): void {
-  for (let i = 0; i < length; i++) {
-    const s1 = load<f32>(a1$)
-    const s2 = load<f32>(a2$)
-    const sample = s1 / s2
-    store<f32>(out$, sample)
-    out$ += 4
-    a1$ += 4
-    a2$ += 4
+  const len = length >> 2
+  for (let i = 0; i < len; i++) {
+    const v1 = v128.load(a1$)
+    const v2 = v128.load(a2$)
+    v128.store(out$, f32x4.div(v1, v2))
+    out$ += 16
+    a1$ += 16
+    a2$ += 16
   }
 }
 
@@ -187,39 +189,42 @@ export function logicAndAudio(out$: usize, a1$: usize, a2$: usize, length: i32):
 // @ts-ignore
 // @inline
 export function bitOrAudio(out$: usize, a1$: usize, a2$: usize, length: i32): void {
-  for (let i = 0; i < length; i++) {
-    const a = i32(load<f32>(a1$))
-    const b = i32(load<f32>(a2$))
-    store<f32>(out$, f32(a | b))
-    out$ += 4
-    a1$ += 4
-    a2$ += 4
+  const len = length >> 2
+  for (let i = 0; i < len; i++) {
+    const v1 = v128.load(a1$)
+    const v2 = v128.load(a2$)
+    v128.store(out$, v128.or(v1, v2))
+    out$ += 16
+    a1$ += 16
+    a2$ += 16
   }
 }
 
 // @ts-ignore
 // @inline
 export function bitXorAudio(out$: usize, a1$: usize, a2$: usize, length: i32): void {
-  for (let i = 0; i < length; i++) {
-    const a = i32(load<f32>(a1$))
-    const b = i32(load<f32>(a2$))
-    store<f32>(out$, f32(a ^ b))
-    out$ += 4
-    a1$ += 4
-    a2$ += 4
+  const len = length >> 2
+  for (let i = 0; i < len; i++) {
+    const v1 = v128.load(a1$)
+    const v2 = v128.load(a2$)
+    v128.store(out$, v128.xor(v1, v2))
+    out$ += 16
+    a1$ += 16
+    a2$ += 16
   }
 }
 
 // @ts-ignore
 // @inline
 export function bitAndAudio(out$: usize, a1$: usize, a2$: usize, length: i32): void {
-  for (let i = 0; i < length; i++) {
-    const a = i32(load<f32>(a1$))
-    const b = i32(load<f32>(a2$))
-    store<f32>(out$, f32(a & b))
-    out$ += 4
-    a1$ += 4
-    a2$ += 4
+  const len = length >> 2
+  for (let i = 0; i < len; i++) {
+    const v1 = v128.load(a1$)
+    const v2 = v128.load(a2$)
+    v128.store(out$, v128.and(v1, v2))
+    out$ += 16
+    a1$ += 16
+    a2$ += 16
   }
 }
 
@@ -276,11 +281,12 @@ export function notAudio(out$: usize, in$: usize, length: i32): void {
 // @ts-ignore
 // @inline
 export function bitNotAudio(out$: usize, in$: usize, length: i32): void {
-  for (let i = 0; i < length; i++) {
-    const a = i32(load<f32>(in$))
-    store<f32>(out$, f32(~a))
-    out$ += 4
-    in$ += 4
+  const len = length >> 2
+  for (let i = 0; i < len; i++) {
+    const v = v128.load(in$)
+    v128.store(out$, v128.not(v))
+    out$ += 16
+    in$ += 16
   }
 }
 
@@ -302,18 +308,21 @@ export function selectAudio(out$: usize, cond$: usize, then$: usize, else$: usiz
 // @ts-ignore
 // @inline
 export function copyAudio(out$: usize, in$: usize, length: i32): void {
-  for (let i = 0; i < length; i++) {
-    store<f32>(out$, load<f32>(in$))
-    out$ += 4
-    in$ += 4
+  const len = length >> 2
+  for (let i = 0; i < len; i++) {
+    v128.store(out$, v128.load(in$))
+    out$ += 16
+    in$ += 16
   }
 }
 
 // @ts-ignore
 // @inline
 export function fillAudio(out$: usize, value: f32, length: i32): void {
-  for (let i = 0; i < length; i++) {
-    store<f32>(out$, value)
-    out$ += 4
+  const len = length >> 2
+  const v = f32x4.splat(value)
+  for (let i = 0; i < len; i++) {
+    v128.store(out$, v)
+    out$ += 16
   }
 }
