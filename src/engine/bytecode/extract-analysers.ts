649b5c11cb62148151bf11ecd83c203eece95192
stagas <gstagas@gmail.com>
2025-12-31 22:16:08 +0200

    save

--- src/engine/bytecode/extract-analysers.ts
+++ src/engine/bytecode/extract-analysers.ts
@@ -28,23 +28,40 @@ function getAnalyserIndexFromCall(callExpr: any): number {
 }
 
 export function createAnalyserVisitor(refs: AnalyserRef[]) {
+  // Track locations that already have analyser widgets
+  const handledLocations = new Set<string>()
+
   return {
     visitCall(expr: any): void {
       if (expr.callee?.kind === 'ident' && (expr.callee?.name === 'out' || expr.callee?.name === 'solo')) {
         const audioArg = getPosArgValue(expr, 0)
         if (audioArg?.kind === 'call' && audioArg.callee?.kind === 'ident' && audioArg.callee?.name === 'analyser') {
-          refs.push({
-            analyserIndex: getAnalyserIndexFromCall(audioArg),
-            loc: expr.callee.loc ?? expr.loc,
-          })
+          const analyserIndex = getAnalyserIndexFromCall(audioArg)
+          const loc = audioArg.callee.loc ?? audioArg.loc
+          const locKey = `${loc?.line}_${loc?.column}`
+
+          if (!handledLocations.has(locKey)) {
+            handledLocations.add(locKey)
+            refs.push({
+              analyserIndex,
+              loc,
+            })
+          }
         }
       }
 
       if (expr.callee?.kind === 'ident' && expr.callee?.name === 'analyser') {
-        refs.push({
-          analyserIndex: getAnalyserIndexFromCall(expr),
-          loc: expr.callee.loc ?? expr.loc,
-        })
+        const analyserIndex = getAnalyserIndexFromCall(expr)
+        const loc = expr.callee.loc ?? expr.loc
+        const locKey = `${loc?.line}_${loc?.column}`
+
+        if (!handledLocations.has(locKey)) {
+          handledLocations.add(locKey)
+          refs.push({
+            analyserIndex,
+            loc,
+          })
+        }
       }
     }
   }
