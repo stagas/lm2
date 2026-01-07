dce3ff773e2b1c33c0cf4425d6587ce7c6d2169e
stagas <gstagas@gmail.com>
2026-01-08 01:10:21 +0200

    save

--- src/engine/intro-program.ts
+++ src/engine/intro-program.ts
@@ -1,4 +1,4 @@
 export const INTRO_PROGRAM = `
 trig=at(1)
-sine(hz-hz+35531 (0 100k)*sine(4554 (0 10k),trig) *ad(.0017,10.0000,10,trig),trig)*ad(.0002,.1,trig)|>lp($,69.71  +4100000*ad(.001,.0711,10,trig)) |> $+freeverb($,room:.852 ,damp:.15)*.6   |> limiter($) |> out($)
+sine(hz-hz+35631 (0 100k)*sine(4554 (0 10k),trig) *ad(.0001,10.0000,10,trig),trig)*ad(.0004,.1,trig)|>lp($,69.71  +200000*ad(.001,.0711,10,trig)) |> $+chorus($,voices:4,depth:.0011,rate:.008,spread:.5)*.5 |>  $+velvet($,room:.770 ,damp:.35,decay:.65)*.6 |> limiter($)*.5 |> out($)
 `
